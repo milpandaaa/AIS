ab4e5812f2f732799bac81edeeee4920eff1f33a
milpandaaa <milpandaaa@bk.ru>
2021-01-02 15:21:34 +0300

    Initial commit

---
+++ src/main/java/com/ais/project/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.ais.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+class MainController{
+
+    @GetMapping("/")
+    public String main(Model model) {//всегда принимает модель
+        model.addAttribute("title", "Главная страница");//передаем параметры
+        return "main";//переход на шаблон main.html
+    }
+
+}
