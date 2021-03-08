49b54f1e147404cc1e60fce81d1003757ba3e1b8
Aleksei Mironov <amironov@jfusion.ru>
2021-03-08 11:08:06 +0300

    fix imports add docker-compose gradle 8.3.1

--- src/main/java/com/ais/project/controller/MainController.java
+++ src/main/java/com/ais/project/controller/MainController.java
@@ -8,8 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.Array;
-import java.sql.Statement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
